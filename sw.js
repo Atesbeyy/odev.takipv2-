849222bca7d68df33327c7ee6ff944468dc2ab90
Atesbeyy <atessepetci811@gmail.com>
2026-09-22 20:57:25 +0300

    Refactor comments in sw.js for clarity

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const ASSETS = [
   './icon.png'
 ];
 
-// Service Worker Yükleme ve Dosyaları Önbelleğe Alma (Caching)
+// Service Worker Yükleme ve Dosyaları Önbelleğe Alma
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -16,7 +16,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// Eski Önbellekleri Temizleme (Activation)
+// Eski Önbellekleri Temizleme
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -32,7 +32,7 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// Ağ İsteklerini Yakalama ve Çevrimdışı Desteği (Fetch Handling)
+// Ağ İsteklerini Yakalama ve Çevrimdışı Desteği
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
